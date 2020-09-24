9cfc826d23d13c8839fef8b2bffbd370ab064fa1
maicolvn1399 <55199282+maicolvn1399@users.noreply.github.com>
2020-09-24 14:13:28 -0600

    metodos de envio de mensajes. aceptar conexión y de escuchar han sido agregados

--- ChatApplicationServer/src/serverchat/ClientThread.java
+++ ChatApplicationServer/src/serverchat/ClientThread.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.LinkedList;
 
 /**
  *Class that makes threads, these threads listen to what the clients say 
@@ -76,10 +77,110 @@ public class ClientThread extends Thread{
                 listening = false;
                 
             }catch(IOException ex){
-                System.err.println("Error when closing client");
+                System.err.println("Error when closing the communication with the client");
                 
         }//End catch
     }//End disconnect()
         
+        /**
+         * Overwrite of the Thread, here's where the infinite cycle is assembled
+         */
+        public void run(){
+            try{
+                listen();
+            }catch(Exception ex){
+                System.out.println("");
+            }//End catch
+            disconnect();
+        }//End run()
         
+        /**
+         * Method that is constantly listening to everything that is sent by the client 
+         */
+        public void listen(){
+            listening = true;
+            while(listening){
+                try{
+                    Object aux = objectInputStream.readObject();
+                    if(aux instanceof LinkedList){
+                        execute((LinkedList<String>)aux);
+                    }//End if 
+                }catch(Exception ex){
+                    System.err.println("Error, the object sent by the client could not be read");
+                }//End catch
+            }//End while
+        }//End listen()
+        
+        /**
+         * Method that makes different actions according to what the socket
+         * has received
+         * @param list
+         */
+        public void execute(LinkedList<String> list){
+            // 0 - the first element is the type 
+            String type = list.get(0);
+            switch(type){
+                case "CONNECTION_REQUEST":
+                    // 1 - ID of the new user
+                    confirmConnection(list.get(1));
+                    break;
+                case "DISCONNECTION_REQUEST":
+                    // 1 - ID of the new user
+                    confirmDisconnection();
+                    break;
+                case "MESSAGE":
+                    // 1 - Sender 
+                    // 2 - Receiver 
+                    // 3 - Message
+                    String receiver = list.get(2);
+                    server.clients
+                            .stream()
+                            .filter(h -> (receiver.equals(h.getID())))
+                            .forEach((h) -> h.sendMessage(list));
+                    break;
+                default:
+                    break;
+       
+            }//End switch 
+        }//End execute 
+        
+        /**
+         * Method to send a message through the socket 
+         * @param list 
+         */
+        private void sendMessage(LinkedList<String> list){
+            try{
+                objectOutputStream.writeObject(list);
+            }catch(Exception ex){
+                System.err.println("There was an error while sending the message to the client");
+            }//End catch
+        }//End sendMessage()
+        
+        /**
+         * Once a new client is connected, this method notifies all the clients in the room 
+         * that there's a new client to be added to their contacts
+         * @param ID
+         */
+        private void confirmConnection(String ID){
+            Server.correlative++;
+            this.ID = Server.correlative+" - "+ID;
+            LinkedList<String> list = new LinkedList<>();
+            list.add("CONNECTION_ACCEPTED");
+            list.add(this.ID);
+            list.addAll(server.getConnectedUsers());
+            sendMessage(list);
+            server.addLog("NEW_USER_CONNECTED");
+            //send all the clients the name of the new user connected except for the user himself
+            LinkedList<String> auxList = new LinkedList<>();
+            auxList.add("NEW_USER_CONNECTED");
+            auxList.add(this.ID);
+            server.clients
+                    .stream()
+                    .forEach(client -> client.sendMessage(auxList));
+            server.clients.add(this);
+            
+                  
+        }//End confirmConnection
+                
+                
 }//End class 
